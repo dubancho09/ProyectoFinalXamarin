cad28afff5e47171b282386998b3229fa294a0c6
09dubancho <duban5555@gmail.com>
2023-03-17 09:32:10 -0500

    Add project files.

---
+++ ProyectoFinalXamarin/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoFinalXamarin.Views;
+using Xamarin.Forms;
+
+namespace ProyectoFinalXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new InsertPerson());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ProyectoFinalXamarin/Views/InsertPerson.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InsertPerson : ContentPage
+    {
+        public InsertPerson()
+        {
+            InitializeComponent();
+        }
+    }
+}
